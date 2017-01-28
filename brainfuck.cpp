10862b17330c76b86cbd22ca480b495ddc6ab989
DaveRoox <davexrusso@gmail.com>
2017-01-28 11:53:54 +0100

    Add files via upload

---
+++ brainfuck.cpp
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <cstdlib>
+
+void brainfuckInterpreter(const char *program) {
+	#define TAPE_SIZE 1000
+	char command, *const tape = (char *const)calloc(TAPE_SIZE, sizeof(char));
+	int brackets, activeCell = 0, programIndex = 0;
+	do {
+		command = program[programIndex];
+		if(command == '>') activeCell++;
+		else if(command == '<') activeCell--;
+		else if(command == '+') tape[activeCell]++;
+		else if(command == '-') tape[activeCell]--;
+		else if(command == '.') printf("%c", tape[activeCell]);
+		else if(command == '_') printf("%d", tape[activeCell]); /* Command to print the char as int (not in the language) */
+		else if(command == ',') scanf("%d", tape + activeCell);
+		else if(command == '[') {
+			if(!tape[activeCell]) {
+				brackets = 1;
+				while(program[++programIndex])
+					if(program[programIndex]== ']' && !--brackets) break;
+					else if(program[programIndex] == '[') brackets++;
+			}
+		}
+		else if(command == ']') {
+			if(tape[activeCell]) {
+				brackets = 1;
+				while(--programIndex >= 0)
+					if(program[programIndex] == '[' && !--brackets) break;
+					else if(program[programIndex] == ']') brackets++;
+			}
+		}
+	} while(programIndex >= 0 && program[++programIndex]);
+	free(tape);
+}
+
+int main() {
+	char nonDestructiveCopy[] = "[>+>+<<-]>>[<<+>>-]<<";
+	char helloWorld[] =
+	"\
+		++++++++++\
+		[\
+		   >+++++++>++++++++++>+++>+<<<<-\
+		]\
+		>++. H\
+		>+. e\
+		+++++++. l\
+		. l\
+		+++. o\
+		>++.\
+		<<+++++++++++++++.\
+		>.\
+		+++.\
+		------.\
+		--------.\
+		>+.\
+		>.\
+	";
+	char mult[] = ", >, <[> ([>+>+<<-]>[<+>-]>) [>+<-] <<<-] >>>>_";
+	brainfuckInterpreter(nonDestructiveCopy);
+	brainfuckInterpreter(helloWorld);
+	printf("Insert your numbers:\n-> ");
+	brainfuckInterpreter(mult);
+	return 0;
+}
